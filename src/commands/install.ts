78747dfedf905d6b71aabe89ba559a5811e16926
erikoleary <erik.m.oleary@gmail.com>
2019-11-30 17:12:18 -0600

    Check for dependencies

--- src/commands/install.ts
+++ src/commands/install.ts
@@ -1,20 +1,50 @@
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
+import * as util from 'util';
+import * as semver from 'semver';
+
+const exec = util.promisify(child_process.exec);
 
 export const register =  (context: vscode.ExtensionContext) => {
 
-    let disposable = vscode.commands.registerCommand('dotnetscriptr.install', (context) => {
-        // Check if 'dotnet-script' is installed
-        // `dotnet tool list -g`
-
-        vscode.window.showInformationMessage("Checking for dotnet");
-        vscode.window.showInformationMessage("Checking for dotnet-script");
-
-        // Use the console to output diagnostic information (console.log) and errors (console.error)
-        // This line of code will only be executed once when your extension is activated
-        console.log('Congratulations, your extension "dotnetscriptr" is now active!');
-
+    let disposable = vscode.commands.registerCommand('dotnetscriptr.install', async (context) => {
+        const output = vscode.window.createOutputChannel("dotnetscriptr - Installing dependencies...");
         
+        try {
+            vscode.window.showInformationMessage("dotnetscriptr - Checking dependencies...");
+
+            output.append("Checking for dotnet core... ");
+
+            const dotnetVersion = (await exec(`dotnet --version`)).stdout;
+            const isValid = semver.gte(dotnetVersion, "3.0.100");
+
+            if (!isValid) {
+                output.appendLine("Missing!");
+                vscode.window.showErrorMessage(`Install dotnet core sdk 3.0 or later at https://dot.net`);
+                return;
+            }
+            else
+                output.appendLine("OK!");
+
+            output.append("Checking for dotnet script... ");
+            
+            await exec(`dotnet tool install -g dotnet-script`)
+                .then(() => output.appendLine("Installed!"))
+                .catch(e => {
+                    if (/already installed/.test(e))
+                        output.appendLine("OK!");
+                    else {
+                        vscode.window.showErrorMessage(`Install dotnet script: ${e}`);
+                        output.appendLine("Error!");
+                    }
+                });
+
+            vscode.window.showInformationMessage("dotnetscriptr - Dependencies OK");
+        }
+        finally {
+            output.dispose();
+        }
     });
     
     context.subscriptions.push(disposable);
-}
+} 
